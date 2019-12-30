e3a6b25b091613d287a32ec0652b91d7298824e1
Jake Hamilton <jakehamtexas@gmail.com>
2019-12-30 11:46:23 -0600

    clippy

--- day-1/challenge-1/fuel_summation/src/main.rs
+++ day-1/challenge-1/fuel_summation/src/main.rs
@@ -1,26 +1,26 @@
 use std::fs;
 
 fn main() {
-    let inputs_raw = read_from_file();
+    let inputs_raw = read_from_file("../../input.txt");
     let masses = parse_as_vector(inputs_raw);
     let total_fuel = get_total_fuel_from_masses(masses);
 
     println!("{}", total_fuel);
 }
 
-fn read_from_file() -> String {
-    return fs::read_to_string("../../input.txt").expect("input.txt doesn't exist!");
+fn read_from_file(relative_input_path: &str) -> String {
+    fs::read_to_string(relative_input_path).expect("input.txt doesn't exist!")
 }
 
 fn parse_as_vector(inputs_raw: String) -> Vec<i32> {
-    return inputs_raw
+    inputs_raw
         .split_ascii_whitespace()
         .map(|m| m.parse().unwrap())
-        .collect();
+        .collect()
 }
 
 fn get_total_fuel_from_masses(masses: Vec<i32>) -> i32 {
-    return masses
+    masses
         .iter()
-        .fold(0, |total_fuel, mass| total_fuel + (mass / 3) - 2);
+        .fold(0, |total_fuel, mass| total_fuel + (mass / 3) - 2)
 }
